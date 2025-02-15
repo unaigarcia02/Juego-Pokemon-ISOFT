b16c0c85d002f794936ce0f5522d0c3a4127ac67
unaigarcia02 <113663442+unaigarcia02@users.noreply.github.com>
2025-02-15 13:26:15 +0100

    Add files via upload

---
+++ controlador/ProgramaPrincipal.java
@@ -0,0 +1,10 @@
+package controlador;
+
+import vista.PantallaPrincipal;
+
+public class ProgramaPrincipal {
+	
+	public static void main(String[] args) {
+		PantallaPrincipal.getPantallaPrincipal();
+	}
+}
